8edfd1157fcee1deee0ceb8106ef5c7c1903638e
atanas-zahariev <atanaszaharieff@abv.bg>
2024-01-30 22:35:35 +0200

    Expand testing of edit.

--- my-app/src/app/actions/edit/edit.component.spec.ts
+++ my-app/src/app/actions/edit/edit.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick, waitForAsync } from '@angular/core/testing';
 
 import { EditComponent } from './edit.component';
 import { HttpClient, HttpHandler } from '@angular/common/http';
@@ -11,6 +11,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Location } from '@angular/common';
 import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 describe('EditComponent', () => {
   let component: EditComponent;
@@ -24,29 +25,29 @@ describe('EditComponent', () => {
   let id: any
 
   beforeEach(waitForAsync(() => {
-    const mockItemService = jasmine.createSpyObj('ItemService', ['details', 'offer']);
+    const mockItemService = jasmine.createSpyObj('ItemService', ['details', 'offer', 'edit']);
     const mockErrorService = jasmine.createSpyObj('ErrorService', ['cleanErrors', 'getError']);
     const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
     TestBed.configureTestingModule({
 
-      imports:[
+      imports: [
         HttpClientTestingModule,
         ReactiveFormsModule,
       ],
 
       declarations: [EditComponent],
 
-      providers:[
+      providers: [
         { provide: ItemsService, useValue: mockItemService },
         { provide: ErrorService, useValue: mockErrorService },
-        { provide: ActivatedRoute, useValue: { snapshot: { params: [id] } } },
+        { provide: ActivatedRoute, useValue: { snapshot: { params: { id: 1 } } } },
         { provide: Router, useValue: routerSpy },
       ],
 
-      schemas:[NO_ERRORS_SCHEMA]
+      schemas: [NO_ERRORS_SCHEMA]
     });
-   
+
   }));
 
   beforeEach(() => {
@@ -83,9 +84,128 @@ describe('EditComponent', () => {
   })
 
   it('should create', () => {
-    const section = fixture.nativeElement.querySelector('section')
-    console.log(section);
-    console.log(component.editForm)
     expect(component).toBeTruthy();
   });
+
+  it('should be right properties value to editForm', () => {
+    const title = component.editForm.get('title')
+    const img = component.editForm.get('imgUrl')
+    const category = component.editForm.get('category')
+    const price = component.editForm.get('price')
+    const description = component.editForm.get('description')
+
+    expect(title?.value).toBe('Motorcycle')
+    expect(img?.value).toBe('https://')
+    expect(category?.value).toBe('vehicles')
+    expect(price?.value).toBe('8314')
+    expect(description?.value).toBe('some motorcycle description')
+  })
+
+  it('should bind the ITEM to its FormControl', () => {
+    const title = fixture.nativeElement.querySelectorAll('input')[0]
+    const img = fixture.nativeElement.querySelectorAll('input')[1]
+    const price = fixture.nativeElement.querySelectorAll('input')[2]
+    const updateButton = fixture.nativeElement.querySelectorAll('input')[3]
+    const category = fixture.nativeElement.querySelector('select')
+    const description = fixture.nativeElement.querySelector('textarea')
+
+    expect(title.value).toBe('Motorcycle')
+    expect(category.value).toBe('vehicles')
+    expect(img.value).toBe('https://')
+    expect(price.value).toBe('8314')
+    expect(updateButton.value).toBe('Update Listing')
+    expect(description.value).toBe('some motorcycle description')
+  })
+
+  it('should change form inputs values', fakeAsync(() => {
+    const title = fixture.nativeElement.querySelectorAll('input')[0]
+    const img = fixture.nativeElement.querySelectorAll('input')[1]
+    const price = fixture.nativeElement.querySelectorAll('input')[2]
+    const category = fixture.nativeElement.querySelector('select')
+    const description = fixture.nativeElement.querySelector('textarea')
+
+    const titleValue = 'Motorcycle!!!'
+    const imgValue = 'https://12456.jpg'
+    const priceValue = '8320'
+    const categoryValue = 'furniture'
+    const descriptionValue = 'some new motorcycle description'
+
+    title.value = titleValue
+    img.value = imgValue
+    price.value = priceValue
+    category.value = category.options[2].value
+    description.value = descriptionValue
+
+    title.dispatchEvent(new Event('input'))
+    img.dispatchEvent(new Event('input'))
+    price.dispatchEvent(new Event('input'))
+    category.dispatchEvent(new Event('change'));
+    description.dispatchEvent(new Event('change'));
+
+    tick()
+    fixture.detectChanges()
+
+    expect(title.value).toEqual(titleValue)
+    expect(img.value).toEqual(imgValue)
+    expect(price.value).toEqual(priceValue)
+    expect(category.value).toEqual(categoryValue)
+    expect(description.value).toEqual(descriptionValue)
+  }))
+
+  it('should call onSubmit', fakeAsync(() => {
+    const form = fixture.debugElement.query(By.css('form'))
+    const func = spyOn(component, 'onSubmit');
+
+    fixture.detectChanges();
+
+    form.triggerEventHandler('ngSubmit', null)
+    tick()
+    fixture.detectChanges();
+
+    expect(func).toHaveBeenCalled();
+  }))
+
+  it('should call edit when form event fires', () => {
+    const form = fixture.debugElement.query(By.css('form'))
+    itemService.edit.and.returnValue(of(true))
+
+    fixture.detectChanges();
+
+    form.triggerEventHandler('ngSubmit', null)
+
+    fixture.detectChanges();
+
+    expect(errorService.cleanErrors).toHaveBeenCalled();
+    expect(itemService.edit).toHaveBeenCalled();
+  })
+
+  it('should navigate after form event fire', () => {
+    const form = fixture.debugElement.query(By.css('form'))
+    const page = new Page()
+    itemService.edit.and.returnValue(of(true))
+
+    fixture.detectChanges();
+
+    form.triggerEventHandler('ngSubmit', null)
+
+    fixture.detectChanges();
+
+    const navArgs = page.navSpy.calls.first().args[0];
+    console.log(navArgs);
+    
+    expect(errorService.cleanErrors).toHaveBeenCalled()
+    expect(page.navSpy.calls.any()).withContext('navigate called').toBe(true);
+    expect(navArgs[0]).withContext('nav to Home URL').toContain('/action/details/1');
+  })
+
+  class Page {
+    /** Spy on router navigate method */
+    navSpy: jasmine.Spy;
+
+    constructor() {
+      // Get the component's injected router navigation spy
+      const routerSpy = fixture.debugElement.injector.get(Router);
+      this.navSpy = routerSpy.navigate as jasmine.Spy;
+    }
+  }
 });
