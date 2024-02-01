b94bd736299606170e9ecb2b29538cdf9ab8093f
atanas-zahariev <atanaszaharieff@abv.bg>
2024-02-01 14:09:56 +0200

    Add test for errorService.

--- my-app/src/app/services/error.service.spec.ts
+++ my-app/src/app/services/error.service.spec.ts
@@ -13,4 +13,31 @@ describe('ErrorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('getError() should set Error to array if argument is string', () => {
+    const error = 'error'
+    service.getError(error)
+
+    expect(service.Error).toEqual(['error'])
+  })
+
+  it('getError() should set Error to array if argument is array', () => {
+    const error = ['error']
+    service.getError(error)
+
+    expect(service.Error).toEqual(error)
+  })
+
+  it('getError() should set Error to array if argument is object', () => {
+    const error = {}
+    service.getError(error)
+
+    expect(service.Error).toEqual(['There seems to be a problem please try again later'])
+  })
+
+  it('should clean Error after cleanErrors is called', () => {
+    service.Error = ['error']
+    service.cleanErrors()
+    expect(service.Error).toEqual([])
+  })
 });
