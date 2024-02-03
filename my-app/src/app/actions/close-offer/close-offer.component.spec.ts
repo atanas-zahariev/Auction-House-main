3ad90f17b4b79d0a7c36b6eb00c0cb7540aabee8
atanas-zahariev <atanaszaharieff@abv.bg>
2024-02-03 12:11:00 +0200

    Finish close-offer component testing.

--- my-app/src/app/actions/close-offer/close-offer.component.spec.ts
+++ my-app/src/app/actions/close-offer/close-offer.component.spec.ts
@@ -1,26 +1,91 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CloseOfferComponent } from './close-offer.component';
 import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ItemsService } from 'src/app/services/items.service';
+import { ErrorService } from 'src/app/services/error.service';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { of, throwError } from 'rxjs';
+
 
 describe('CloseOfferComponent', () => {
   let component: CloseOfferComponent;
   let fixture: ComponentFixture<CloseOfferComponent>;
+  let httpClient: HttpClient;
+  let itemService: jasmine.SpyObj<ItemsService>;
+  let errorService: jasmine.SpyObj<ErrorService>;
+  let router: Router;
+  let location: Location;
+  let activatedRoute: ActivatedRoute;
+  let id: any
+  let navArgs: any
+
+  beforeEach(waitForAsync(() => {
+    const mockItemService = jasmine.createSpyObj('ItemService', ['details', 'offer', 'closeOffer']);
+    const mockErrorService = jasmine.createSpyObj('ErrorService', ['cleanErrors', 'getError']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+    TestBed.configureTestingModule({
+
+      declarations: [CloseOfferComponent],
+      providers: [
+        { provide: ItemsService, useValue: mockItemService },
+        { provide: ErrorService, useValue: mockErrorService },
+        { provide: ActivatedRoute, useValue: { snapshot: { params: { id: '1' } } } },
+        { provide: Router, useValue: routerSpy },
+      ],
+
+      schemas: [NO_ERRORS_SCHEMA],
+
+      imports: [
+        HttpClientTestingModule,
+      ],
+    }).compileComponents();
+
+  }));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports:[RouterTestingModule],
-      declarations: [CloseOfferComponent],
-      providers:[HttpClient,HttpHandler,RouterTestingModule]
-    });
-    fixture = TestBed.createComponent(CloseOfferComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    httpClient = TestBed.inject(HttpClient);
 
-  it('should create', () => {
+    itemService = TestBed.inject(ItemsService) as jasmine.SpyObj<ItemsService>;
+
+    errorService = TestBed.inject(ErrorService) as jasmine.SpyObj<ErrorService>;
+
+    router = TestBed.inject(Router);
+
+    location = TestBed.inject(Location);
+
+    activatedRoute = TestBed.inject(ActivatedRoute);
+
+    itemService.closeOffer.and.returnValue(of(true));
+
+    fixture = TestBed.createComponent(CloseOfferComponent);
+
+    component = fixture.componentInstance;
+    
+    fixture.detectChanges();
+  })
+
+  it('should create and navigate', () => {
+    fixture.detectChanges
     expect(component).toBeTruthy();
   });
+
+  it('should call getError', () => {
+    itemService.closeOffer.and.returnValue(throwError(() => new Error()));
+
+    fixture = TestBed.createComponent(CloseOfferComponent);
+
+    component = fixture.componentInstance;
+    
+    fixture.detectChanges();
+
+    expect(errorService.getError).toHaveBeenCalled()
+  })
+
 });
