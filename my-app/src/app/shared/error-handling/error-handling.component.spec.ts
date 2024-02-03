d21c07b53c0b7c6ac7827336b7c9a8d65de20d7c
atanas-zahariev <atanaszaharieff@abv.bg>
2024-02-03 17:08:44 +0200

    Finish error-handling component testing.

--- my-app/src/app/shared/error-handling/error-handling.component.spec.ts
+++ my-app/src/app/shared/error-handling/error-handling.component.spec.ts
@@ -1,21 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErrorHandlingComponent } from './error-handling.component';
+import { ErrorService } from 'src/app/services/error.service';
 
 describe('ErrorHandlingComponent', () => {
   let component: ErrorHandlingComponent;
   let fixture: ComponentFixture<ErrorHandlingComponent>;
+  let errorService: ErrorService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ErrorHandlingComponent]
+      declarations: [ErrorHandlingComponent],
+      providers: [
+         ErrorService
+      ]
     });
+    
+    errorService = TestBed.inject(ErrorService)
     fixture = TestBed.createComponent(ErrorHandlingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
+  
   it('should create', () => {
+    errorService.Error = ['error']
+    fixture.detectChanges()
+    const pError = fixture.nativeElement.querySelector('p')
+    
     expect(component).toBeTruthy();
+    expect(pError.textContent).toEqual('error')
   });
 });
